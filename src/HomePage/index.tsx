e1b00eee01f9526d056ad45978d95c3a977a1582
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-05-29 07:12:35 -0300

    Definindo estrutura dos cards

--- src/HomePage/index.tsx
+++ src/HomePage/index.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import TitleCustom from "../components/TitleCustom";
 
 export default function HomePage() {
   return (
-    <div>
-      <h1>HomePage</h1>
-    </div>
+    <main className="container mx-auto p-4">
+      <TitleCustom>Home</TitleCustom>
+      <section
+        id="products"
+        className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
+      >
+        <div className="w-full max-w-xs bg-dark-surface rounded-2xl shadow-md p-4">
+          <div className="w-full h-40 bg-border-muted rounded-lg ">
+            <img
+              src={"src/assets/images/produto.jpg"}
+              alt={"produto.name"}
+              className="w-full h-40 object-cover rounded-lg"
+            />
+          </div>
+          <h2 className="text-lg font-semibold text-text-primary mt-2">
+            produto
+          </h2>
+          <p className="text-sm text-text-secondary">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+          <p className="text-primary-btn font-bold text-base mt-2">R$ 89,90</p>
+        </div>
+      </section>
+    </main>
   );
 }
