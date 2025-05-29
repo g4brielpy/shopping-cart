c2b19d5a0b71317d284f9250dfac9b687884e198
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-05-29 07:41:36 -0300

    Corrigindo altura da página

--- src/layouts/LayoutRoot.tsx
+++ src/layouts/LayoutRoot.tsx
@@ -3,7 +3,7 @@ import { HeaderPage } from "../components/HeaderPage";
 
 export default function LayoutRoot() {
   return (
-    <div className="bg-dark-bg h-dvh text-primary-text">
+    <div className="bg-dark-bg min-h-dvh text-primary-text">
       <HeaderPage />
       <Outlet />
     </div>
