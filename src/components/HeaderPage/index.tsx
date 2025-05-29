6be7c43ed7fd31c4182ee4e262b63155d519e470
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-05-29 20:37:12 -0300

    Adicionando links de navegação

--- src/components/HeaderPage/index.tsx
+++ src/components/HeaderPage/index.tsx
@@ -1,16 +1,21 @@
-import { HiMenuAlt4 } from "react-icons/hi";
+import { GrCart } from "react-icons/gr";
 import { FaReact } from "react-icons/fa";
+
+import { Link } from "react-router";
 
 export function HeaderPage() {
   return (
     <header className="container mx-auto flex justify-between items-center p-4 bg-lue-600 font-bold text-xl md:text-2xl lg:text-3xl">
-      <span className="flex items-center gap-2">
+      <Link to={"/"} className="flex items-center gap-2">
         <FaReact />
         Shopping
-      </span>
-      <button className="bg-border-muted hover:bg-border-muted/70 transition-all p-2 rounded">
-        <HiMenuAlt4 />
-      </button>
+      </Link>
+      <Link
+        to={"carrinho/"}
+        className="bg-border-muted hover:bg-border-muted/70 transition-all p-2 rounded"
+      >
+        <GrCart />
+      </Link>
     </header>
   );
 }
