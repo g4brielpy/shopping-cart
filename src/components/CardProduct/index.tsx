1bc7754bc8953317a707902de1936b34b8ce0c42
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-05-29 20:56:22 -0300

    Premitir class personalizadas

--- src/components/CardProduct/index.tsx
+++ src/components/CardProduct/index.tsx
@@ -2,7 +2,8 @@ interface CardProductProps {
   title: string;
   description: string;
   price: string;
-  imgSrc: string;
+  imgSrc?: string;
+  className?: string;
 }
 
 export function CardProduct({
@@ -10,17 +11,27 @@ export function CardProduct({
   description,
   price,
   imgSrc,
+  className = "",
 }: CardProductProps) {
   return (
-    <div className="w-full max-w-xs min-h-[320px] bg-dark-surface rounded-2xl shadow-md p-4 cursor-pointer">
-      <div className="w-full h-40 bg-border-muted rounded-lg ">
-        <img
-          src={imgSrc}
-          alt={title}
-          className="w-full h-40 object-cover rounded-lg"
-        />
-      </div>
-      <div className="flex flex-col gap-2 mt-4">
+    <div
+      className={`
+        w-full max-w-xs  bg-dark-surface rounded-2xl shadow-md p-4 cursor-pointer ${
+          imgSrc && "min-h-[320px]"
+        } 
+        ${className}
+       `}
+    >
+      {imgSrc && (
+        <div className="w-full h-40 bg-border-muted rounded-lg mb-4">
+          <img
+            src={imgSrc}
+            alt={title}
+            className="w-full h-40 object-cover rounded-lg"
+          />
+        </div>
+      )}
+      <div className="flex flex-col gap-2">
         <h2 className="text-lg font-semibold text-text-primary mt-2">
           {title}
         </h2>
