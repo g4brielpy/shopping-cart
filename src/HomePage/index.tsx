450f9d172657153840fc5eab21bec7beaecb5def
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-05-29 07:48:41 -0300

    Container para cards

--- src/HomePage/index.tsx
+++ src/HomePage/index.tsx
@@ -1,4 +1,5 @@
 import TitleCustom from "../components/TitleCustom";
+import { CardProduct } from "../components/CardProduct";
 
 export default function HomePage() {
   return (
@@ -6,24 +7,56 @@ export default function HomePage() {
       <TitleCustom>Home</TitleCustom>
       <section
         id="products"
-        className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
+        className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-items-center"
       >
-        <div className="w-full max-w-xs bg-dark-surface rounded-2xl shadow-md p-4">
-          <div className="w-full h-40 bg-border-muted rounded-lg ">
-            <img
-              src={"src/assets/images/produto.jpg"}
-              alt={"produto.name"}
-              className="w-full h-40 object-cover rounded-lg"
-            />
-          </div>
-          <h2 className="text-lg font-semibold text-text-primary mt-2">
-            produto
-          </h2>
-          <p className="text-sm text-text-secondary">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-          </p>
-          <p className="text-primary-btn font-bold text-base mt-2">R$ 89,90</p>
-        </div>
+        <CardProduct
+          title="produto"
+          description="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+          price="R$ 89,90"
+          imgSrc="src/assets/images/produto.jpg"
+        />
+        <CardProduct
+          title="produto"
+          description="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+          price="R$ 89,90"
+          imgSrc="src/assets/images/produto.jpg"
+        />
+        <CardProduct
+          title="produto"
+          description="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+          price="R$ 89,90"
+          imgSrc="src/assets/images/produto.jpg"
+        />
+        <CardProduct
+          title="produto"
+          description="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+          price="R$ 89,90"
+          imgSrc="src/assets/images/produto.jpg"
+        />
+        <CardProduct
+          title="produto"
+          description="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+          price="R$ 89,90"
+          imgSrc="src/assets/images/produto.jpg"
+        />
+        <CardProduct
+          title="produto"
+          description="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+          price="R$ 89,90"
+          imgSrc="src/assets/images/produto.jpg"
+        />
+        <CardProduct
+          title="produto"
+          description="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+          price="R$ 89,90"
+          imgSrc="src/assets/images/produto.jpg"
+        />
+        <CardProduct
+          title="produto"
+          description="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+          price="R$ 89,90"
+          imgSrc="src/assets/images/produto.jpg"
+        />
       </section>
     </main>
   );
