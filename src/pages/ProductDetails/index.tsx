b8ac7f1fbdb7983ab0a27a1f0a7f0b7d0e4491a2
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-06-06 07:28:34 -0300

    Página de detalhes de produtos

--- src/pages/ProductDetails/index.tsx
+++ src/pages/ProductDetails/index.tsx
@@ -2,12 +2,16 @@ import { CardProduct } from "../../components/CardProduct";
 
 export default function ProductDetails() {
   return (
-    <CardProduct
-      title="produto"
-      description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem, ipsum dolor sit amet consectetur adipisicing elit."
-      price="R$ 89,90"
-      imgSrc="src/assets/images/produto.jpg"
-      className="line-clamp-none"
-    />
+    <main className="container h-full mx-auto my-20 p-4 flex items-center justify-center">
+      <div className="border border-primary-btn rounded-3xl p-4">
+        <CardProduct
+          title="produto"
+          description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+          price="R$ 89,90"
+          imgSrc="src/assets/images/produto.jpg"
+          className="line-clamp-none"
+        />
+      </div>
+    </main>
   );
 }
