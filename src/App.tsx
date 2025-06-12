d2eb2ed2a86e55207c6544c180b8f8dc843c20c8
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-06-12 07:13:14 -0300

    Adicionar Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { RouterProvider } from "react-router";
 import { Router } from "./Routes";
+import { ProductsProvider } from "./contexts/ProductsProvider";
 
 function App() {
-  return <RouterProvider router={Router} />;
+  return (
+    <ProductsProvider>
+      <RouterProvider router={Router} />
+    </ProductsProvider>
+  );
 }
 
 export default App;
