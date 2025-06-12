4e0c59853dac9d6fbe8f63a64baba0b72f6b0f93
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-06-12 07:24:44 -0300

    Consumindo  useContext para trazer todos os produtos

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,7 +1,12 @@
 import TitleCustom from "../../components/TitleCustom";
 import { CardProduct } from "../../components/CardProduct";
 
+import { useContext } from "react";
+import { ProductsContext } from "../../contexts/ProductsContext";
+
 export default function HomePage() {
+  const productsData = useContext(ProductsContext);
+
   return (
     <main className="container mx-auto p-4">
       <TitleCustom>Home</TitleCustom>
@@ -9,54 +14,18 @@ export default function HomePage() {
         id="products"
         className="my-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-items-center"
       >
-        <CardProduct
-          title="produto"
-          description="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
-          price="R$ 89,90"
-          imgSrc="src/assets/images/produto.jpg"
-        />
-        <CardProduct
-          title="produto"
-          description="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
-          price="R$ 89,90"
-          imgSrc="src/assets/images/produto.jpg"
-        />
-        <CardProduct
-          title="produto"
-          description="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
-          price="R$ 89,90"
-          imgSrc="src/assets/images/produto.jpg"
-        />
-        <CardProduct
-          title="produto"
-          description="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
-          price="R$ 89,90"
-          imgSrc="src/assets/images/produto.jpg"
-        />
-        <CardProduct
-          title="produto"
-          description="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
-          price="R$ 89,90"
-          imgSrc="src/assets/images/produto.jpg"
-        />
-        <CardProduct
-          title="produto"
-          description="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
-          price="R$ 89,90"
-          imgSrc="src/assets/images/produto.jpg"
-        />
-        <CardProduct
-          title="produto"
-          description="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
-          price="R$ 89,90"
-          imgSrc="src/assets/images/produto.jpg"
-        />
-        <CardProduct
-          title="produto"
-          description="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
-          price="R$ 89,90"
-          imgSrc="src/assets/images/produto.jpg"
-        />
+        {productsData.map((product) => (
+          <CardProduct
+            key={product.id}
+            title={product.title}
+            description={product.description}
+            price={product.price.toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            })}
+            imgSrc={product.image}
+          />
+        ))}
       </section>
     </main>
   );
