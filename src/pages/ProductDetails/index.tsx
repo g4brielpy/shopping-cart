4dbded192e0993b28ab7dfe8c1cdcdb7448e5f38
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-06-20 13:39:36 -0300

    Formatando valor do produto

--- src/pages/ProductDetails/index.tsx
+++ src/pages/ProductDetails/index.tsx
@@ -1,16 +1,44 @@
 import { CardProduct } from "../../components/CardProduct";
+import { useParams } from "react-router";
+
+import { useContext, useEffect, useState } from "react";
+import { ProductsContext, ProductProps } from "../../contexts/ProductsContext";
 
 export default function ProductDetails() {
+  const { productId } = useParams();
+  const productsData = useContext(ProductsContext);
+  const [product, setProduct] = useState<ProductProps>({} as ProductProps);
+
+  useEffect(() => {
+    const productData = productsData.find(
+      (product) => product.id.toString() === productId
+    );
+
+    setProduct(productData!);
+  }, [productId, productsData]);
+
+  let valorFormatado = "R$ 0,00";
+  if (typeof product.price === "number") {
+    valorFormatado = product.price.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+
+    console.log("Valor formatado:", valorFormatado);
+  }
+
   return (
     <main className="container h-full mx-auto my-20 p-4 flex items-center justify-center">
       <div className="border border-primary-btn rounded-3xl p-4">
-        <CardProduct
-          title="produto"
-          description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem, ipsum dolor sit amet consectetur adipisicing elit."
-          price="R$ 89,90"
-          imgSrc="src/assets/images/produto.jpg"
-          className="line-clamp-none"
-        />
+        {product && (
+          <CardProduct
+            title={product.title}
+            description={product.description}
+            price={valorFormatado}
+            imgSrc={product.image}
+            className="line-clamp-none"
+          />
+        )}
       </div>
     </main>
   );
