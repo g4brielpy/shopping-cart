ba21f31439ca5d43924ba9a30d56bb098d52f209
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-06-22 14:47:02 -0300

    Corrigindo função de remover items

--- src/contexts/CartProvider.tsx
+++ src/contexts/CartProvider.tsx
@@ -2,20 +2,29 @@ import { CartContext } from "./CartContext";
 import { ProductProps } from "./ProductsContext";
 import { useState } from "react";
 
-function addToCart() {
-  console.log("Adicionar item");
-}
-
-function removeFromCart() {
-  console.log("Remover item");
-}
-
-function clearCart() {
-  console.log("Limpar carrinho");
-}
-
 export function CartProvider({ children }: { children: React.ReactNode }) {
   const [cartItems, setCartItems] = useState<ProductProps[]>([]);
+
+  function addToCart(item: ProductProps) {
+    setCartItems((oldCartItems) => [...oldCartItems, item]);
+    console.log("Item adicionado com sucesso!");
+  }
+
+  function removeFromCart(item: ProductProps) {
+    const indexToRemove = cartItems.findIndex(
+      (product) => product.id == item.id
+    );
+
+    const newCartItems = [...cartItems];
+    newCartItems.splice(indexToRemove, 1);
+
+    console.log(newCartItems);
+    setCartItems(newCartItems);
+  }
+
+  function clearCart() {
+    console.log("Limpar carrinho");
+  }
 
   return (
     <CartContext.Provider
