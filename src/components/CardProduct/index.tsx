40772f4f661c9b24c6ec8db86ee69e61ac885d3e
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-07-03 07:14:37 -0300

    Corrigindo estilização

--- src/components/CardProduct/index.tsx
+++ src/components/CardProduct/index.tsx
@@ -3,6 +3,7 @@ import { CartContext } from "../../contexts/CartContext";
 import { ProductProps } from "../../types/product";
 
 import { IoIosAdd } from "react-icons/io";
+import { Link } from "react-router";
 
 interface CardCartProps {
   product: ProductProps;
@@ -15,34 +16,37 @@ export function CardProduct({ product, className = "" }: CardCartProps) {
   return (
     <div
       className={`
-        w-full max-w-xs  bg-dark-surface rounded-2xl shadow-md p-4 cursor-pointer ${
-          product.image && "min-h-[320px]"
-        } 
+        w-full max-w-xs  bg-dark-surface rounded-2xl shadow-md p-4 h-[380px] flex flex-col
         ${className}
        `}
     >
-      {product.image && (
-        <div className="w-full h-40 bg-white rounded-lg mb-4">
+      <Link title="Ver detalhes do produto" to={`produto/${product.id}`}>
+        <div className="w-full h-40 bg-white rounded-lg mb-4 cursor-pointer">
           <img
             src={product.image}
             alt={product.title}
             className="w-full h-40 object-contain rounded-lg"
           />
         </div>
-      )}
-      <div className="flex flex-col gap-2">
-        <h2 className="text-lg font-semibold text-text-primary mt-2 line-clamp-2">
+      </Link>
+      <div className="bg-amber-30 flex-1 flex flex-col justify-between gap-2">
+        <h2 className="text-lg font-semibold text-text-primary line-clamp-2">
           {product.title}
         </h2>
         <p className="text-sm text-secondary-text line-clamp-2">
           {product.description}
         </p>
-        <div className="flex items-center justify-between mt-4">
-          <p className="font-extrabold text-base mt-2">{product.price}</p>
+        <div className="flex items-center justify-between">
+          <p className="font-extrabold text-base">
+            {product.price.toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            })}
+          </p>
 
           <button
             onClick={() => CartData?.addToCart(product)}
-            title="Adicionar ao carrinho"
+            title="Adicionar item ao carrinho"
             className="text-secondary-text bg-border-muted p-2 rounded cursor-pointer hover:bg-gray-700 transition-colors"
           >
             <IoIosAdd size={20} />
