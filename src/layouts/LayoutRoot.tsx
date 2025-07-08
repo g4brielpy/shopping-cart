bcdebe7ed23f30b862c5b5bc3aac93aa8a9331f4
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2025-07-07 21:03:07 -0300

    Criando rodapé

--- src/layouts/LayoutRoot.tsx
+++ src/layouts/LayoutRoot.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router";
+
 import { HeaderPage } from "../components/HeaderPage";
+import { FooterPage } from "../components/FooterPage";
 
 export default function LayoutRoot() {
   return (
     <div className="bg-dark-bg min-h-dvh text-primary-text">
       <HeaderPage />
       <Outlet />
+      <FooterPage />
     </div>
   );
 }
